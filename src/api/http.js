b2f5a7d70d441d746fa1dbabe0e38bc72f08d2b3
KingJem <qqqqivy@gmail.com>
2020-11-07 09:30:12 +0800

    ‘’

--- src/api/http.js
+++ src/api/http.js
@@ -2,10 +2,10 @@ import axios from 'axios';
 import qs from 'qs'
 
 // 环境的切换
-process.env.NODE_ENV =='development'
+process.env.NODE_ENV === 'development'
 
 
-switch(process.env.NODE_ENV){
+switch (process.env.NODE_ENV) {
     case 'production':
         axios.defaults.baseURL = 'http://localhost:3000/api';
         break;
@@ -30,31 +30,31 @@ axios.defaults.transformRequest = qs.stringify(data);
 // 设置请求拦截器
 
 // 响应拦截器
-axios.interceptors.response.use(    
-    response => {   
+axios.interceptors.response.use(
+    response => {
         // 如果返回的状态码为200，说明接口请求成功，可以正常拿到数据     
         // 否则的话抛出错误
-        if (response.status === 200) {            
-            return Promise.resolve(response);        
-        } else {            
-            return Promise.reject(response);        
-        }    
-    },    
+        if (response.status === 200) {
+            return Promise.resolve(response);
+        } else {
+            return Promise.reject(response);
+        }
+    },
     // 服务器状态码不是2开头的的情况
     // 这里可以跟你们的后台开发人员协商好统一的错误状态码    
     // 然后根据返回的状态码进行一些操作，例如登录过期提示，错误提示等等
     // 下面列举几个常见的操作，其他需求可自行扩展
-    error => {            
-        if (error.response.status) {            
-            switch (error.response.status) {                
+    error => {
+        if (error.response.status) {
+            switch (error.response.status) {
                 // 401: 未登录
                 // 未登录则跳转登录页面，并携带当前页面的路径
                 // 在登录成功后返回当前页面，这一步需要在登录页操作。                
-                case 401:                    
-                    router.replace({                        
-                        path: '/login',                        
-                        query: { 
-                            redirect: router.currentRoute.fullPath 
+                case 401:
+                    router.replace({
+                        path: '/login',
+                        query: {
+                            redirect: router.currentRoute.fullPath
                         }
                     });
                     break;
@@ -64,7 +64,7 @@ axios.interceptors.response.use(
                 // 清除本地token和清空vuex中token对象
                 // 跳转登录页面                
                 case 403:
-                     Toast({
+                    Toast({
                         message: '登录过期，请重新登录',
                         duration: 1000,
                         forbidClick: true
@@ -73,15 +73,15 @@ axios.interceptors.response.use(
                     localStorage.removeItem('token');
                     store.commit('loginSuccess', null);
                     // 跳转登录页面，并将要浏览的页面fullPath传过去，登录成功后跳转需要访问的页面 
-                    setTimeout(() => {                        
-                        router.replace({                            
-                            path: '/login',                            
-                            query: { 
-                                redirect: router.currentRoute.fullPath 
-                            }                        
-                        });                    
-                    }, 1000);                    
-                    break; 
+                    setTimeout(() => {
+                        router.replace({
+                            path: '/login',
+                            query: {
+                                redirect: router.currentRoute.fullPath
+                            }
+                        });
+                    }, 1000);
+                    break;
 
                 // 404请求不存在
                 case 404:
@@ -101,14 +101,12 @@ axios.interceptors.response.use(
             }
             return Promise.reject(error.response);
         }
-    } );   
+    });
 
 
-
-axios.get(url,  {
-    params: {}
-}
-
+axios.get(url, {
+        params: {}
+    }
 ).then(response => {
 
 }).catch(err => {
